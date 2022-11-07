3f0329fa9a89d7b48374450e6d5a8d9631e1435e
CorentinGS <c.giaufersaubert@outlook.com>
2022-11-07 22:51:56 +0100

    :memo: Header

--- src/menu.h
+++ src/menu.h
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Copyright (c) 2022.                                                        *
+ *                                                                            *
+ *                                                                            *
+ * Game engine                            ______   _______      _______.      *
+ * menu.h                                /      | /  _____|    /       |      *
+ *                                      |  ,----'|  |  __     |   (----`      *
+ * By: CorentinGS                       |  |     |  | |_ |     \   \          *
+ * <c.giaufersaubert@outlook.com>       |  `----.|  |__| | .----)   |         *
+ *                                       \______| \______| |_______/  (_/ -/  *
+ * LICENCE: MIT                                                        /  /   *
+ * Created: 2022/11/7 by CorentinGS                                           *
+ *                                                                            *
+ ******************************************************************************/
+
 #include "common.h"
 
 extern void readScoreboard(const char* filename, Scoreboard tab[]);
--- src/text.h
+++ src/text.h
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * Copyright (c) 2022.                                                        *
+ *                                                                            *
+ *                                                                            *
+ * Game engine                            ______   _______      _______.      *
+ * text.h                                /      | /  _____|    /       |      *
+ *                                      |  ,----'|  |  __     |   (----`      *
+ * By: CorentinGS                       |  |     |  | |_ |     \   \          *
+ * <c.giaufersaubert@outlook.com>       |  `----.|  |__| | .----)   |         *
+ *                                       \______| \______| |_______/  (_/ -/  *
+ * LICENCE: MIT                                                        /  /   *
+ * Created: 2022/11/7 by CorentinGS                                           *
+ *                                                                            *
+ ******************************************************************************/
+
 #include "common.h"
 
 extern void blitRect(SDL_Texture* texture, SDL_Rect* src, int x, int y);
